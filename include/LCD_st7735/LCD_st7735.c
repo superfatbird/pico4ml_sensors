ad5f4cebae2c4e7f90cb5e9b57f073a19815b6e0
jeffOldBoy <100885502+jeffOldBoy@users.noreply.github.com>
2022-04-11 17:43:46 +0800

    Fix st7735 screen not displayed, and update uf2 file

--- include/LCD_st7735/LCD_st7735.c
+++ include/LCD_st7735/LCD_st7735.c
@@ -226,8 +226,8 @@ static void ST7735_SetAddressWindow(uint8_t x0, uint8_t y0, uint8_t x1,
 
 void ST7735_Init() {
   DEV_Module_Init();
-  ST7735_Select();
   ST7735_Reset();
+  ST7735_Select();
   ST7735_ExecuteCommandList(init_cmds1);
   ST7735_ExecuteCommandList(init_cmds2);
   ST7735_ExecuteCommandList(init_cmds3);
